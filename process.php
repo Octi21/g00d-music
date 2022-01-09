39640b1a6947d33e272f9b329093bdeb67c89447
Octi21 <80587811+Octi21@users.noreply.github.com>
2022-01-09 17:10:09 +0200

    Add files via upload

--- process.php
+++ process.php
@@ -1,5 +1,5 @@
 <?php
-
+    use PHPMailer\PHPMailer\PHPMailer;
     require_once('connection.php');
 
     if(isset($_POST['title']) && !empty($_POST['title']))                                           // bot detector
@@ -24,29 +24,75 @@
             }
             else
             {
-            
-                if($Cpassword != $Password)
+                $sql0 = "select * from utilizatori where UName = '$UserName' or Email = '$Email'";  
+                $result0 = mysqli_query($con,$sql0);
+                if($row = mysqli_fetch_assoc($result0))
                 {
-                    echo ' wrong pass';
+                    echo "error nu poti folosi acest username sau acest email";
                 }
+
                 else
                 {
-                    $pass = md5($Password);
-                    $sql = "insert into utilizatori (Uname,Email,Password,Grad) values ('$UserName','$Email','$pass','client')";
-
-                    $result = mysqli_query($con,$sql);
-
-                    if($result)
+                    //verific parola
+                    if($Cpassword != $Password)
                     {
-                        
-                        header('location:login.php');
-                        // echo '<script type="text/javascript"> alert("User registered ... Go to login ") </script>';
+                        echo ' wrong pass';
                     }
                     else
                     {
-                        echo 'error3';
+                        // trimite mail
+                        
+                        
+                        require_once "PHPMailer/PHPMailer.php";
+                        require_once "PHPMailer/SMTP.php";
+                        require_once "PHPMailer/Exception.php";
+
+                        $mail = new PHPMailer();
+
+                        //SMTP Settings
+                        $mail -> isSMTP();
+                        $mail -> Host = "smtp.gmail.com";
+                        $mail -> SMTPAuth = true;
+                        $mail -> Username = "godmusic212@gmail.com";
+                        $mail -> Password = "Goodmusic2121";
+                        $mail ->Port = 587;
+                        $mail ->SMTP = "tls";
+
+                        //email set
+                        $mail -> isHTML(isHtml:true);
+                        $mail -> setFrom($Email, "good-music");
+                        $mail -> addAddress(address:"$Email");
+                        $mail -> Subject = "Inregistrare pe good-music";
+                        $mail -> Body = "Bun venit!";
+
+                        if($mail ->send())
+                        {
+                            echo "===   Email trimis    ===";
+                        }
+                        else
+                        {
+                            echo "error";
+                        }
+
+                        // creare cont
+                        $pass = md5($Password);
+                        $sql = "insert into utilizatori (Uname,Email,Password,Grad) values ('$UserName','$Email','$pass','client')";
+
+                        $result = mysqli_query($con,$sql);
+
+                        if($result)
+                        {
+                            
+                            header('location:login.php');
+                            // echo '<script type="text/javascript"> alert("User registered ... Go to login ") </script>';
+                        }
+                        else
+                        {
+                            echo 'error3';
+                        }
                     }
                 }
+
             }
         }
     }
